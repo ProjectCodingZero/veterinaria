f75ee8d30234d3e43d0be511bdc6a248f059b42e
unknown <m.rieznik17@gmail.com>
2021-07-31 13:29:17 -0300

    Agregando Biblioteca y ejercicio 2

---
+++ operaciones.h
@@ -0,0 +1,27 @@
+int suma (int numa, int numb)
+{
+	int sum;
+	sum = numa+numb;
+	return sum;
+}
+
+int resta (int numa, int numb)
+{
+	int res;
+	res = numa-numb;
+	return res;
+}
+
+int multiplicacion (int numa, int numb)
+{
+	int mult;
+	mult = numa*numb;
+	return mult;
+}
+
+float division (int numa, int numb)
+{
+	float divi;
+	divi = numa/numb;
+	return divi;
+}
