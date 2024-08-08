97c34edc75abda8cd53658c961e7f211304cc4ea
PoP <cuentafeik1009@gmail.com>
2024-08-08 13:52:56 -0400

    trying to configure backend

---
+++ backend/src/main/java/veterinaria/backend/VeterinariaBackendApplication.java
@@ -0,0 +1,13 @@
+package veterinaria.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinariaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinariaBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/veterinaria/backend/VeterinariaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package veterinaria.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
